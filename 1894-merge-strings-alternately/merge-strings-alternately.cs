fdd9e24d9521c157d56df38c28e076f8a06f3ccd
abdullahhh-bey <ryk42069@gmail.com>
2025-08-02 15:29:11 +0500

    Time: 72 ms (18.87%) | Memory: 41.4 MB (21.59%) - LeetSync

---
+++ 1894-merge-strings-alternately/merge-strings-alternately.cs
@@ -0,0 +1,34 @@
+public class Solution {
+    public string MergeAlternately(string word1, string word2) {
+        string result = "";
+        bool turn = false;
+        int j = 0;
+        int k = 0;
+        int size = word1.Length + word2.Length;
+
+            for(int i = 0; i < size ; i++)
+            {
+                if(turn == false && k != word1.Length)
+                {
+                        result = result + word1[k];
+                        turn = true;   
+                        k++;   
+
+                } else if(turn == true && j != word2.Length) {
+                        result = result + word2[j];
+                        turn = false;
+                        j++;
+                } else if(k < word1.Length)
+                {
+                    result = result + word1[k];
+                    k++;
+                } else if(j < word2.Length){
+                    result = result + word2[j];
+                    j++;
+                }
+            }  
+
+        return result;
+
+    }
+}
