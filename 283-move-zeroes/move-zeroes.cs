8364dac65de39d9ce03667b3950901c2f56cf5b2
abdullahhh-bey <ryk42069@gmail.com>
2025-08-04 01:12:44 +0500

    Time: 16 ms (14.61%) | Memory: 58.9 MB (10.71%) - LeetSync

--- 283-move-zeroes/move-zeroes.cs
+++ 283-move-zeroes/move-zeroes.cs
@@ -1,10 +1,15 @@
 public class Solution {
     public void MoveZeroes(int[] nums) {
         
+        //I was comfortable using List, could be done without them
         List<int> num = nums.ToList();
+        //took the size of original array/list for loop iteration
         int size = num.Count;
         int k = 0;
 
+        //Logic is to iterate every element and find 0, if 0 found , remove it from
+        //its psition and add it to the back ( at last ) 
+        //If the item is non zero, do k++
         for(int i = 0 ; i < size ; i++)
         {
             if(num[k] == 0)
@@ -15,7 +20,9 @@ public class Solution {
                 k++;
             }
         }
+
         
+        //updating the array ( making it similar to list )
         for(int i = 0 ; i < size ; i++)
         {
             nums[i] = num[i];
