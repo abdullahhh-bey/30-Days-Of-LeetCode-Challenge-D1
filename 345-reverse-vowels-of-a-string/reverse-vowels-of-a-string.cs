d066f301277fe88e237d5cabce2712840b3f0caa
abdullahhh-bey <ryk42069@gmail.com>
2025-08-03 16:58:42 +0500

    Time: 2 ms (99.42%) | Memory: 45.1 MB (89.50%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cs
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cs
@@ -1,7 +1,16 @@
 public class Solution {
     public string ReverseVowels(string s) {
+
+        //Changing the String to char Array
+        //I can also change it to List<char> but the array is better in this case
         char[] str = s.ToCharArray();
+
+        //Declared k for indexing of string from the back
         int k = str.Length - 1;
+
+        //Logic is to Start from the 0th index and check if there is vowel, track from back and check if
+        //there is any voiwel, if yes then swap them , do k-- and break the inner loop and 
+        //check again fdor the outer loop 
         for(int i = 0 ; i <= k ; i++)
         {
             if(str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u' 
@@ -23,6 +32,8 @@ public class Solution {
                 }
             }
         }
+
+        //it returns the char array as a string
         return new string(str);
 
     }
