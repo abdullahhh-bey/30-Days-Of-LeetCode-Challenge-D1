e0ea785db01dc7e76adb9c3b167f50c791206f28
abdullahhh-bey <ryk42069@gmail.com>
2025-08-04 15:56:36 +0500

    Time: 0 ms (100.00%) | Memory: 40.1 MB (94.26%) - LeetSync

---
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+
+        //I used c++ just to practice
+        vector<int> result(nums.size());
+
+        //Using common approach - first find prefix & add it to result array and then find the suffix
+        //and multity it to the prefix of ith item
+        //it would be like
+        //result[ith] = prodiuct of left items of result[i] * product of right items of result[i]
+
+        //Assume first index a 1
+        result[0] = 1;
+        for(int i = 1; i < nums.size(); i++)
+        {
+            result[i] = result[i - 1] * nums[i - 1];
+        }
+
+        //Assuming Suffix as 1 
+        int suffix = 1;
+        for(int j = nums.size() - 1; j >= 0; j--)
+        {
+            result[j] = result[j] * suffix;
+            suffix *= nums[j];
+        }
+
+        //Return the Updated Array
+        return result;
+        
+        }
+};
