d6ae77ff6a323b67c4c9f70df581e6383d30b1b0
abdullahhh-bey <ryk42069@gmail.com>
2025-08-05 15:17:25 +0500

    Time: 43 ms (19.18%) | Memory: 50 MB (14.35%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cs
@@ -0,0 +1,36 @@
+using System.Text.RegularExpressions;
+
+public class Solution {
+    public bool IsPalindrome(string s) {
+        //Converting into Lowercase
+        //Removed the non-alphanumerci characters
+        string cleanedStr = Regex.Replace(s, "[^a-zA-Z0-9]", "").ToLower();
+        //Declared a array of characters for storing the reverse of cleanedStr
+        List<char> reverseStr = new List<char>();
+        List<char> updated = new List<char>();
+
+        reverseStr = cleanedStr.ToList();
+
+        if(reverseStr.Count == 0 || reverseStr.Count == 1)
+        {
+            return true;
+        }
+
+
+        for(int  i = cleanedStr.Length - 1 ; i >= 0 ; i-- )
+        {
+            updated.Add(reverseStr[i]);
+        }
+
+        string s1 = new string(reverseStr.ToArray());
+        string s2 = new string(updated.ToArray());
+
+        if(s1 == s2)
+        {
+            return true;
+        } else{
+            return false;
+        }
+
+    }
+}
