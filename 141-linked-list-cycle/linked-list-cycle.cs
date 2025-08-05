a138f238107ccb7b264f565b93fc9a7f97eb52e5
abdullahhh-bey <ryk42069@gmail.com>
2025-08-05 22:42:21 +0500

    Time: 106 ms (42.14%) | Memory: 47.6 MB (60.15%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cs
+++ 141-linked-list-cycle/linked-list-cycle.cs
@@ -13,44 +13,73 @@
 public class Solution {
     public bool HasCycle(ListNode head) {
 
-        //Mad Approach
-        //Going to each node and storing its unique address in a list
-        //and if any node contains the already reached address in its next pointer
-        //it means it has a cycle
+        // //Mad Approach
+        // //Going to each node and storing its unique address in a list
+        // //and if any node contains the already reached address in its next pointer
+        // //it means it has a cycle
 
-        //List to store the Addresses
-        List<ListNode> address = new List<ListNode>();
+        // //List to store the Addresses
+        // List<ListNode> address = new List<ListNode>();
 
-        //Checks for validation
-        if(head == null)
+        // //Checks for validation
+        // if(head == null)
+        // {
+        //     return false;
+        // } 
+
+        // //For Strarting while loop
+        // bool foundCycle = false;
+        // //POinter that points to the head ad then used for traversal
+        // ListNode current = head;
+
+        // //Adding the address of the very first node ( head )
+        // address.Add(head);
+
+        // //Main Logic
+        // while(foundCycle == false)
+        // {
+        //     if(current.next == null ){
+        //         break;
+        //     } else if(address.Contains(current.next))
+        //     {
+        //         return true;
+        //     }else {
+        //         address.Add(current.next);
+        //         current = current.next;
+        //     }
+        // }
+
+        // //If no cycle is found, return false
+        //     return false;
+
+
+        //Floyd Tortuse&Rabbit Approach
+        //Making a fast pointer ahead of the slow pointer
+        //If fast pointer meets slow pointer at some pointer. it means there is a cycle, hence 
+        //return true
+        
+        if(head == null || head.next == null)
         {
             return false;
-        } 
-
-        //For Strarting while loop
-        bool foundCycle = false;
-        //POinter that points to the head ad then used for traversal
-        ListNode current = head;
-
-        //Adding the address of the very first node ( head )
-        address.Add(head);
-
-        //Main Logic
-        while(foundCycle == false)
-        {
-            if(current.next == null ){
-                break;
-            } else if(address.Contains(current.next))
-            {
-                return true;
-            }else {
-                address.Add(current.next);
-                current = current.next;
-            }
         }
 
-        //If no cycle is found, return false
-            return false;
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        //It there is no cycle, this condition will become false in upcoming iterations
+        while(fast != null && fast.next != null)
+        {
+            //If equals, then it means there is a cycle
+            if(fast == slow)
+            {
+                return true;
+            }
+
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return false;
 
     }
 }
