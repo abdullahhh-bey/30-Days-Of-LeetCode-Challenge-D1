b2ef591bd5b6673082a408d4b0ded6323ac4404d
abdullahhh-bey <ryk42069@gmail.com>
2025-08-06 22:21:25 +0500

    Time: 10 ms (6.56%) | Memory: 47.5 MB (47.05%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.cs
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cs
@@ -1,9 +1,16 @@
 public class Solution {
     public int[] Intersection(int[] nums1, int[] nums2) {
+
+        //Converting the Arrays into LIST
         List<int> n1 = nums1.ToList();
         List<int> n2 = nums2.ToList();
+
+        //It will store the result
         List<int> result = new List<int>();
 
+        //Main logic is that i will go to each element of list1/array1
+        //and check if it is present in list2/array2, it yes then i will check that 
+        //if it is present in resultList, if yes then skip and if not then add it to result
         for(int i = 0 ; i < n1.Count ; i++)
         {
            if(n2.Contains(n1[i]) && !result.Contains(n1[i]))
@@ -12,6 +19,7 @@ public class Solution {
            }
         }
 
+        //Converting the List into Aarray
         int[] n = result.ToArray();
         return n;
     }
