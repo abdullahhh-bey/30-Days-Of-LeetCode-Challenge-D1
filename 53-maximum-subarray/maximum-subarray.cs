cc8019f87af9f49d28080e82c229b4eb9071e186
abdullahhh-bey <ryk42069@gmail.com>
2025-08-06 15:46:23 +0500

    Time: 3 ms (8.93%) | Memory: 64 MB (51.04%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cs
+++ 53-maximum-subarray/maximum-subarray.cs
@@ -1,10 +1,17 @@
 public class Solution {
     public int MaxSubArray(int[] nums) {
+
+        //Approach is very simplE
+        //Variable to store the sum of two or more numbers
         int sum = 0;
+        //It iwll store the max sum
         int maxSum = 0;
+
+        //Used for validation
         bool neg = false;
         int max = -1000000;
 
+        //Validation method , this is for the  arrays that have only negative numbers
         for(int i = 0 ; i < nums.Length ; i++)
         {
             if(nums[i] < 0)
@@ -22,6 +29,10 @@ public class Solution {
 
 
 
+        //Main Logic starts from here
+        //Check for each item, add it to sum, check if the sum is greater than zero, if yes them compare
+        //COmpoare it to the previous maximum value
+        //If sum is smaller than zero or equal to zero, reset the sum because it is not necessary
         for(int i = 0 ; i < nums.Length ; i++)
         {
             sum += nums[i];
