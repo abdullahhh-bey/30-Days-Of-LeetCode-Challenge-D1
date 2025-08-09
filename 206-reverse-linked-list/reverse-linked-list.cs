ed2271a21f6bf934ad439bf14eeabad6c85ea814
abdullahhh-bey <ryk42069@gmail.com>
2025-08-09 18:27:33 +0500

    Time: 1 ms (3.92%) | Memory: 42.6 MB (79.22%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.cs
+++ 206-reverse-linked-list/reverse-linked-list.cs
@@ -11,20 +11,28 @@
  */
 public class Solution {
     public ListNode ReverseList(ListNode head) {
+
+        //Defining 3 pointers 
+        //temp for next 
+        //current for iteration
+        //forwatrd for the previous
         ListNode temp = head;
         ListNode current = head;
         ListNode forward = null;
 
+        //Chevcking  if its null or not
         if( head == null)
         {
             return null;
         }
-        
+
         if(head.next == null )
         {
             return head;
         }
 
+        //This is the main logic and its very simple
+        //I used two pointer approach to solvethis 
         while(current != null)
         {
             temp = current.next;
@@ -33,7 +41,7 @@ public class Solution {
             current = temp; 
         }
 
-       
+            //returning the head of the new reverse linked list 
             return forward;
         
 
