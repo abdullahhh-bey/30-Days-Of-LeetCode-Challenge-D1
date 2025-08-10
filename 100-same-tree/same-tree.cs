85ef85d28dba43909d072354039a13ab51c92a8d
abdullahhh-bey <ryk42069@gmail.com>
2025-08-10 20:56:17 +0500

    Time: 0 ms (100.00%) | Memory: 43.7 MB (50.62%) - LeetSync

---
+++ 100-same-tree/same-tree.cs
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public bool IsSameTree(TreeNode p, TreeNode q) {
+        //it firstr check the validation 
+        //if both are null, return true
+        if( p == null &&   q == null )
+        {
+            return true;
+        }
+
+        //If any one of them is not null, return false
+        if( p == null || q == null )
+        {
+            return false;
+        }
+
+        //Main logic
+        //If the root of both trees is same , CHECK FOR THE LEFT AND RIGHT NODES
+        if(p.val == q.val)
+        {
+            return IsSameTree(p.left , q.left) && IsSameTree(p.right , q.right);        
+        }
+
+        return false;
+
+    }
+}
